6d9a7dc0a2c11cb33a5f248e8166b422aacfbd85
lorenzuou <lorenzoscaramussa@hotmail.com>
2024-05-23 18:45:46 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'app.html'));
+});
+
+app.get('/sam', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'app_with_SAM.html'));
+});
+
+app.get('/box', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'app_with_box.html'));
+});
+
+const port = process.env.PORT || 4002;
+app.listen(port, function() {
+    
+    console.log(`Server is listening on port ${port}`);
+});
