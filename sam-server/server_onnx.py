2e297b2c9fc30c196552b99f475d61790452952a
lorenzuou <lorenzoscaramussa@hotmail.com>
2024-06-18 15:12:29 -0300

    fixing

--- sam-server/server_onnx.py
+++ sam-server/server_onnx.py
@@ -44,6 +44,64 @@ image_embedding = None
 
 onnx_model_path = "sam_onnx_quantized.onnx"
 
+#check if onnx model exists
+try:
+    ort_session = onnxruntime.InferenceSession(onnx_model_path)
+except:
+    print('Onnx model not found, creating it now...')
+    import warnings
+
+    onnx_model_not_quint_path = "sam_onnx_example.onnx"
+
+    onnx_model = SamOnnxModel(sam, return_single_mask=True)
+
+    dynamic_axes = {
+        "point_coords": {1: "num_points"},
+        "point_labels": {1: "num_points"},
+    }
+
+    embed_dim = sam.prompt_encoder.embed_dim
+    embed_size = sam.prompt_encoder.image_embedding_size
+    mask_input_size = [4 * x for x in embed_size]
+    dummy_inputs = {
+        "image_embeddings": torch.randn(1, embed_dim, *embed_size, dtype=torch.float),
+        "point_coords": torch.randint(low=0, high=1024, size=(1, 5, 2), dtype=torch.float),
+        "point_labels": torch.randint(low=0, high=4, size=(1, 5), dtype=torch.float),
+        "mask_input": torch.randn(1, 1, *mask_input_size, dtype=torch.float),
+        "has_mask_input": torch.tensor([1], dtype=torch.float),
+        "orig_im_size": torch.tensor([1500, 2250], dtype=torch.float),
+    }
+    output_names = ["masks", "iou_predictions", "low_res_masks"]
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=torch.jit.TracerWarning)
+        warnings.filterwarnings("ignore", category=UserWarning)
+        with open(onnx_model_not_quint_path, "wb") as f:
+            torch.onnx.export(
+                onnx_model,
+                tuple(dummy_inputs.values()),
+                f,
+                export_params=True,
+                verbose=False,
+                opset_version=17,
+                do_constant_folding=True,
+                input_names=list(dummy_inputs.keys()),
+                output_names=output_names,
+                dynamic_axes=dynamic_axes,
+            )
+
+    onnx_model_quantized_path = "sam_onnx_quantized.onnx"
+    quantize_dynamic(
+        model_input=onnx_model_not_quint_path,
+        model_output=onnx_model_quantized_path,
+        optimize_model=True,
+        per_channel=False,
+        reduce_range=False,
+        weight_type=QuantType.QUInt8,
+    )
+    onnx_model_path = onnx_model_quantized_path
+    ort_session = onnxruntime.InferenceSession(onnx_model_not_quint_path)
+
 
 predictor = SamPredictor(sam)
 
@@ -119,7 +177,6 @@ def predict_box():
         set_image(image_cv2)
     image_embedding = predictor.get_image_embedding().cpu().numpy()
 
-    ort_session = onnxruntime.InferenceSession(onnx_model_path)
     onnx_coord = np.concatenate([onnx_box_coords, np.array([[0.0, 0.0]])], axis=0)[None, :, :]
     onnx_label = np.concatenate([onnx_box_labels, np.array([-1])], axis=0)[None, :].astype(np.float32)
 
@@ -196,7 +253,7 @@ def predict_prompt():
         set_image(image_cv2)
     image_embedding = predictor.get_image_embedding().cpu().numpy()
 
-    ort_session = onnxruntime.InferenceSession(onnx_model_path)
+    # ort_session = onnxruntime.InferenceSession(onnx_model_path)
     onnx_coord = np.concatenate([input_points, np.array([[0.0, 0.0]])], axis=0)[None, :, :]
     onnx_label = np.concatenate([input_labels, np.array([-1])], axis=0)[None, :].astype(np.float32)
 
